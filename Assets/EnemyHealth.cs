0ddd0678b8ec82c0b16f9d4f543488579d19ab4a
tranthihuong-753 <dhhuongdhlt1@gmail.com>
2026-02-01 21:43:00 +0700

    UPDATE part 3

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+public GameObject explosionPrefab;
+
+private void OnTriggerEnter2D(Collider2D collision) => Die();
+
+private void Die()
+{
+var explosion = Instantiate(explosionPrefab, transform.position,
+transform.rotation);
+Destroy(explosion, 1);
+Destroy(gameObject);
+}
+}
---
+++ Assets/PlayerShooting1.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerShooting1 : MonoBehaviour
+{
+public GameObject bulletPrefabs;
+public float shootingInterval;
+public Vector3 bulletOffset;
+
+private float lastBulletTime;
+
+// Update is called once per frame
+void Update()
+{
+if (Input.GetMouseButton(0))
+{
+UpdateFiring();
+}
+}
+
+private void UpdateFiring()
+{
+if (Time.time - lastBulletTime > shootingInterval)
+{
+ShootBullet();
+lastBulletTime = Time.time;
+}
+}
+
+private void ShootBullet()
+{
+var bullet = Instantiate(bulletPrefabs, transform.position + bulletOffset, transform.rotation);
+}
+}
