9d54da952c6b57b9e91aa7efc557a290b688b432
tranthihuong-753 <BIT230192@st.cmc-u.edu.vn>
2026-03-15 22:23:13 +0700

    demo part 7

--- Assets/part6/Health.cs
+++ Assets/part6/Health.cs
@@ -6,16 +6,22 @@ public class Health : MonoBehaviour
 {
     public GameObject explosionPrefab;
     public int defaultHealthPoint;
-    private int healthPoint;
+    public int healthPoint;
     public System.Action onDead;
+    public System.Action onHealthChanged;
 
-    private void Start() => healthPoint = defaultHealthPoint;
+    private void Start()
+    {
+        healthPoint = defaultHealthPoint;
+        onHealthChanged?.Invoke();
+    }
 
     public void TakeDamage(int damage)
     {
         if (healthPoint <= 0) return;
 
         healthPoint -= damage;
+        onHealthChanged?.Invoke();
         if (healthPoint <= 0) Die();
     }
 
